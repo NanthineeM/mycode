4924154c11416b81273411fbdab59fa19d3d263c
Nanthinee <nanthini.m@gmail.com>
2022-06-02 21:00:47 -0400

    sumofoddplace

---
+++ oddplacesum.js
@@ -0,0 +1,9 @@
+//Sum of Digits in Odd places//
+
+var X = 987456;
+
+var Num= X.toString();
+
+var test= parseInt(Num.substring(0,1))+parseInt(Num.substring(2,3))+parseInt(Num.substring(4,5));
+console.log((test));
+
