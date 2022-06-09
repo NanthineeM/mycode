1425ce62e64a6ae35bdff0c839b8afac3b87e2f6
Nanthinee <nanthini.m@gmail.com>
2022-06-09 17:21:18 -0400

    final

--- handson2.js
+++ handson2.js
@@ -1,5 +1,21 @@
-const birds = ['Parrot', 'Falcon', 'Owl'];
 
-for (const bird of birds) {
-  console.log(bird);
-}
+//let str = 'Hi There, how are you?';
+
+//console.log(str.substring(4,10));
+
+//const random=['tree',795,[0,1,2]];
+
+//console.log(random[2][2]);
+
+//const cities = ['Manchester','Liverpool'];
+//const newLength = cities.push('Bristol');
+//console.log(cities);
+//console.log(newLength);
+
+//const birds =['Parrot','Falcon','Owl'];
+//for(const name of birds){
+  //console.log(name);
+//}
+
+
+
